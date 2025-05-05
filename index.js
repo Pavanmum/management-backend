ffa9d7ce91e596d54fd1fcef462409fecce987bf
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2025-05-05 23:07:23 +0530

    Update CORS configuration to allow requests from the management frontend

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ console.log(`MongoDB URI: ${MONGO_URI}`);
 
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://management-frontend-sigma.vercel.app'], 
   credentials: true,               
 }));
 app.use(express.json());
