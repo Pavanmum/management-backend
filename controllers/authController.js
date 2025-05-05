b6786ab83e269c43fc704a61a5d56c4c462973d7
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2025-05-06 00:12:39 +0530

    Enhance response messages in authentication and user management to include success status

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ export const register = async (req, res) => {
         maxAge: 24 * 60 * 60 * 1000,
       });
 
-      res.json({ message: 'Login successful' });
+      res.json({ message: 'Login successful',success:true });
     } catch (err) {
         console.log(err);
       res.status(500).json({ message: 'Internsl Server error',success:false });
@@ -67,7 +67,7 @@ export const register = async (req, res) => {
       });
   
       console.log('Cookie cleared successfully');
-      return res.status(200).json({ message: 'Logout successful' });
+      return res.status(200).json({ message: 'Logout successful',success:true });
     } catch (err) {
       console.error('Logout error:', err.message, err.stack);
       return res.status(500).json({ message: 'Internal Server error' });
@@ -109,12 +109,12 @@ export const register = async (req, res) => {
     try {
       const users = await User.find({role: 'user'}).select('-password -__v');
 
-      if (!users) return res.status(404).json({ message: 'No users found' });
+      if (!users) return res.status(404).json({ message: 'No users found',success: false });
 
       res.status(200).json({ users, success: true });
     } catch (err) {
         console.log(err);
-      res.status(500).json({ message: 'Internsl Server error' });
+      res.status(500).json({ message: 'Internsl Server error',success: false });
     }
   }
 
@@ -133,7 +133,7 @@ export const register = async (req, res) => {
       res.status(200).json({ message: 'User updated', success: true });
     } catch (err) {
         console.log(err);
-      res.status(500).json({ message: 'Internsl Server error' });
+      res.status(500).json({ message: 'Internsl Server error' ,success: false });
     }
   }
 
